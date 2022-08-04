7826c5b281d746fdca2c8d7418d1d34f43a6b630
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-08-04 18:14:11 -0300

    refactor: add additional space for prompts

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,19 +5,19 @@ function installGit() {
 }
 
 function setupGit() {
-  echo -n "$(tput setaf 5)❓️ Enter your name for Git:"; read NAME
+  echo -n "$(tput setaf 5)❓️ Enter your name for Git: "; read NAME
   git config --global user.name $NAME
-  echo -n "$(tput setaf 5)❓️ Enter your email for Git:"; read EMAIL
+  echo -n "$(tput setaf 5)❓️ Enter your email for Git: "; read EMAIL
   git config --global user.email $EMAIL
   echo $GITCONFIG >> ~/.gitconfig
 }
 
 function generateSSHKey() {
-  echo -n "$(tput setaf 5)❓️ Enter your email for SSH Key:"; read SSHKEYEMAIL
+  echo -n "$(tput setaf 5)❓️ Enter your email for SSH Key: "; read SSHKEYEMAIL
   ssh-keygen -t ed25519 -C $SSHKEYEMAIL
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_ed25519
-  echo -n "$(tput setaf 5)❓️ Enter a name for your SSH Key:"; read SSHKEYNAME
+  echo -n "$(tput setaf 5)❓️ Enter a name for your SSH Key: "; read SSHKEYNAME
   gh ssh-key add ~/.ssh/id_ed25519.pub --title $SSHKEYNAME
 }
 
