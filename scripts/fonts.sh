c4958ae8b634879d58fcb6d8589fdbdc8cf8d017
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-08-04 12:37:27 -0300

    fix: update font install command

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 function installNerdFonts() {
+  mkdir fonts_cache
+  cd fonts_cache
   wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraMono.zip
   unzip FiraMono.zip
   wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
   unzip FiraCode.zip
   mkdir ~/.fonts
-  cp FiraMono/* ~/.fonts
-  cp FiraCode/* ~/.fonts
+  rm FiraMono.zip
+  rm FiraCode.zip
+  sudo cp * ~/.fonts
+  cd ..
+  rm -rf fonts_cache
 }
