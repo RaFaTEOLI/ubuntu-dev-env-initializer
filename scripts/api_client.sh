6550b328c71d9aefb00652501c36b9c262c98ceb
Rafael <rafinha.tessarolo@hotmail.com>
2022-09-08 18:22:38 -0300

    fix: update insomnia package url

--- scripts/api_client.sh
+++ scripts/api_client.sh
@@ -5,7 +5,7 @@
 function installInsomnia() {
   mkdir api_client_cache
   cd api_client_cache
-  wget https://updates.insomnia.rest/downloads/ubuntu/latest?&app=com.insomnia.app&source=website
+  wget https://github.com/Kong/insomnia/releases/download/core%402022.5.1/Insomnia.Core-2022.5.1.deb
   INSOMNIAPKG=$(ls | head -1)
   sudo dpkg -i INSOMNIAPKG
 }
