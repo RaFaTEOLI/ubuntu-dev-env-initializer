89537263920c344d2a10703a0c9802af8195536e
Rafael Tessarollo <rafinha.tessarolo@hotmail.com>
2023-10-03 20:09:24 -0300

    chore: update node version to 18

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function installNode() {
-  curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+  curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
   sudo apt update
   sudo apt install nodejs -y
 }
