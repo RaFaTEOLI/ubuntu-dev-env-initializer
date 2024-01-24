9c77bc2f703821a82bd66fb3ab0ba83db6d6ef10
Rafael Tessarollo <rafinha.tessarolo@hotmail.com>
2024-01-24 17:12:07 -0300

    chore: update vscode version

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 function installVSCode() {
-  wget https://az764295.vo.msecnd.net/stable/3b889b090b5ad5793f524b5d1d39fda662b96a2a/code_1.69.2-1658162013_amd64.deb
+  wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/8b3775030ed1a69b13e4f4c628c612102e30a681/code_1.85.2-1705561292_amd64.deb
   sudo dpkg -i code_1.69.2-1658162013_amd64.deb
 }
